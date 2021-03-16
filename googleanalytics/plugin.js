469bbc647ee643c88975c2ec5ddac43e029a6a5a
Signati <52678977+Signati@users.noreply.github.com>
2021-03-16 15:36:22 -0500

    Add files via upload

---
+++ googleanalytics/plugin.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueGtag from 'vue-gtag'
+
+export default (ctx) => {
+  
+    Vue.use(VueGtag, {
+      config: {
+        id: 'G-L96E2TZNTG'
+      },
+      pageTrackerScreenviewEnabled: true,
+      enabled: true
+    }, ctx.app.router)
+
+}

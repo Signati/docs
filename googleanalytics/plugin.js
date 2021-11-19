39bb3ac4ad69440148da841d0cd47293b958de9c
MIsaelMa <amisael.amir.misael@gmail.com>
2021-11-19 10:44:09 -0500

    feat

--- googleanalytics/plugin.js
+++ googleanalytics/plugin.js
@@ -3,12 +3,12 @@ import VueGtag from 'vue-gtag'
 
 export default (ctx) => {
 
-    // Vue.use(VueGtag, {
-    //   config: {
-    //     id: 'G-L96E2TZNTG'
-    //   },
-    //   pageTrackerScreenviewEnabled: true,
-    //   enabled: true
-    // }, ctx.app.router)
+    Vue.use(VueGtag, {
+      config: {
+        id: 'G-L96E2TZNTG'
+      },
+      pageTrackerScreenviewEnabled: true,
+      enabled: true
+    }, ctx.app.router)
 
 }

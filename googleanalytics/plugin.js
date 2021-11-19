557f9093e9acfdfc2ee3f70f9ccaf6ed7f5cba7f
MIsaelMa <amisael.amir.misael@gmail.com>
2021-11-19 09:22:56 -0500

    test

--- googleanalytics/plugin.js
+++ googleanalytics/plugin.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
 export default (ctx) => {
-  
+
     Vue.use(VueGtag, {
       config: {
-        id: 'G-L96E2TZNTG'
+        id: 'UA-212994312-1' // 'G-L96E2TZNTG'
       },
       pageTrackerScreenviewEnabled: true,
       enabled: true
